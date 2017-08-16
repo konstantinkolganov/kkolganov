107d2a03622cb2bfb4474b90407064ee9b4a914b
konstantinkolganov <kkolganov.92@gmail.com>
2017-08-16 12:48:52 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,9 @@
+/** Метод main выводит на экран сообщение "Hello, world"
+* @author Konstantin Kolganov (kkolganov.92@gmail.com)
+* @since 16.08.2017
+* @version 2.0
+*/
+
 package ru.job4j;
 
 public class Calculate {
