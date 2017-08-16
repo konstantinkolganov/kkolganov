615c54b4ac8ae45c90c05e6164a073d6d412766f
konstantinkolganov <kkolganov.92@gmail.com>
2017-08-16 13:02:57 +0300

    task 4087

---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+﻿/** Класс Package-info создан для описания содержимого пакета ru.job4j
+* Класс Calculate предназначен для вычисления арифметических операций "+, -, /, *"
+* @author Konstantin Kolganov (kkolganov.92@gmail.com)
+* @since 16.08.2017
+* @version 2.0
+*/
+
+package ru.job4j;
