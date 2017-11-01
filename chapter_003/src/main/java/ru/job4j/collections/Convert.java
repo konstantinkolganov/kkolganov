f6f819b716a604dcf56fe9becf007ce563c1fe6b
konstantinkolganov <kkolganov.92@gmail.com>
2017-11-01 23:58:41 +0300

    task

---
+++ chapter_003/src/main/java/ru/job4j/collections/Convert.java
@@ -0,0 +1,98 @@
+package ru.job4j.collections;
+
+import java.util.*;
+/**
+* Класс Convert. Конвертация двумерного массива в ArrayList и наборот.
+* @author Konstantin Kolganov (kkolganov.92@gmail.com)
+* @since 31.10.2017
+* @version 2.0
+*/
+
+public class Convert {
+/**
+* Класс Convert.
+*/
+/**
+* Метод main.
+* @param args args.
+*/
+	public static void main(String[] args) {
+		// Новый объект Convert
+		Convert convert = new Convert();
+		// Двумерный целочисленный массив и его инициализация
+		int[][] array = new int[3][3];
+		System.out.println("Elements of Array : ");
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = i * j;
+				System.out.print(array[i][j]);
+			}
+			System.out.println();
+		}
+		// Конвертация массива в список List и вывод на экран
+		List<Integer> list = convert.toList(array);
+		System.out.println("Size of list : " + list.size());
+		System.out.println("Content of list : " + list);
+		// Конвертация списка обратно в массив и вывод его на экран
+		// Массив из двух строк.
+		int[][] array2 = convert.toArray(list, 2);
+		System.out.println("Elements of Array : ");
+		for (int i = 0; i < array2.length; i++) {
+			for (int j = 0; j < array2[i].length; j++) {
+				System.out.print(array2[i][j]);
+			}
+			System.out.println();
+		}
+	}
+/**
+* Метод toList.
+* @param array Двумерный массив целых чисел.
+* @return Коллекцию ArrayList<Integer>.
+*/
+	public List<Integer> toList(int[][] array) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int[] i : array) {
+			for (int j : i) {
+				list.add(j);
+			}
+		}
+		return list;
+	}
+/**
+* Метод toArray.
+* @param list Коллекция List.
+* @param rows Количество строк двумерного массива.
+* @return Двумерный массив, состоящий из элементов List.
+*/
+	public int[][] toArray(List<Integer> list, int rows) {
+		int[][] arRows = null;
+		// Получение массива из элементов коллекции
+		Integer[] array = list.toArray(new Integer[list.size()]);
+		// Число колонок в будущем двумерном массиве
+		int colls = array.length / rows;
+		int index = 0;
+		if (array.length % rows == 0) {
+			arRows = new int[rows][colls];
+			for (int i = 0; i < arRows.length; i++) {
+				for (int j = 0; j < arRows[i].length; j++) {
+					arRows[i][j] = array[index + j].intValue();
+				}
+				index += colls;
+			}
+		} else {
+			colls += 1;
+			arRows = new int[rows][colls];
+			for (int i = 0; i < arRows.length; i++) {
+				for (int j = 0; j < arRows[i].length; j++) {
+					if ((index + j) < array.length) {
+						arRows[i][j] = array[index + j].intValue();
+					} else {
+						arRows[i][j] = 0;
+					}
+				}
+				index += colls;
+			}
+		}
+		return arRows;
+	}
+}
---
+++ chapter_003/src/main/java/ru/job4j/collections/Perfomance.java
@@ -0,0 +1,81 @@
+package ru.job4j.collections;
+
+import java.util.*;
+/**
+* Класс Perfomance замеряет время вставки в коллекцию и время удаления случайных строк.
+* @author Konstantin Kolganov (kkolganov.92@gmail.com)
+* @since 31.10.2017
+* @version 2.0
+*/
+
+public class Perfomance {
+/**
+* Класс Perfomance.
+*/
+	public static final Random RN = new Random(); /** Random. */
+/**
+* Метод main.
+* @param args args.
+*/
+	public static void main(String[] args) {
+		// Новый объект Perfomance
+		Perfomance perf = new Perfomance();
+		// Тест коллекции ArrayList
+		Collection<String> collectArray = new ArrayList<String>();
+		System.out.println("ArrayList :");
+		System.out.println("Time of add : " + perf.add(collectArray, 10000));
+		System.out.println("Time of delete : " + perf.delete(collectArray, 10000));
+		System.out.println();
+		// Тест коллекции LinkedList
+		Collection<String> collectLink = new LinkedList<String>();
+		System.out.println("LinkedList :");
+		System.out.println("Time of add : " + perf.add(collectLink, 10000));
+		System.out.println("Time of delete : " + perf.delete(collectLink, 10000));
+		System.out.println();
+		// Тест коллекции TreeSet
+		Collection<String> collectSet = new TreeSet<String>();
+		System.out.println("TreeSet :");
+		System.out.println("Time of add : " + perf.add(collectSet, 10000));
+		System.out.println("Time of delete : " + perf.delete(collectSet, 10000));
+		System.out.println();
+	}
+/**
+* Метод add. Добавление элементов в коллекцию.
+* @param collection Коллекция, куда будут добавляться элементы.
+* @param amount Количество элементов.
+* @return Время выполнения операции.
+*/
+	public long add(Collection<String> collection, int amount) {
+		// Начало отсчета
+		long start = System.nanoTime();
+		for (int i = 0; i < amount; i++) {
+			collection.add(String.valueOf(System.currentTimeMillis() + RN.nextInt()));
+		}
+		// Конец отсчета
+		long finish = System.nanoTime();
+		// Время выполнения
+		long result = finish - start;
+		return result;
+	}
+/**
+* Метод delete. Удаление элементов из коллекции.
+* @param collection Коллекция, куда будут добавляться элементы.
+* @param amount Количество элементов для удаления.
+* @return Время выполнения операции.
+*/
+	public long delete(Collection<String> collection, int amount) {
+		// Массив для хранения элементов, подлежащих удалению
+		String[] array = new String[amount];
+		array = collection.toArray(array);
+		// Начало отсчета
+		long start = System.nanoTime();
+		for (int i = 0; i < array.length; i++) {
+			collection.remove(array[i]);
+		}
+		// Конец отсчета
+		long finish = System.nanoTime();
+		// Время выполнения
+		long result = finish - start;
+		return result;
+	}
+}
